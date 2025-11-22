8b6cfad55014a68e2ff6ff998f0d43709e37d5c3
linzyspace <linzyspace@gmail.com>
2025-11-22 13:37:16 -0800

    Update and rename cachedPosts.json to defaultResponses.js

---
+++ data/defaultResponses.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { keywords: ["hello", "hi"], reply: "Hello! How can I help you today?" },
+  { keywords: ["how are you"], reply: "I'm doing great, thanks for asking!" },
+  { keywords: ["help", "support"], reply: "Sure! What do you need help with?" },
+  { keywords: ["bye", "goodbye"], reply: "Goodbye! Have a nice day!" },
+];
+
