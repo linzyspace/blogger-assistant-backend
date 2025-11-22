2346a2912125f18c2c9b2fea3d61deb595756def
linzyspace <linzyspace@gmail.com>
2025-11-22 10:03:59 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const chatRoutes = require("./routes/chat");
+const { PORT } = require("./config/config");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use("/chat", chatRoutes);
+
+app.get("/", (req, res) => res.send("Blogger Assistant Backend Running"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
