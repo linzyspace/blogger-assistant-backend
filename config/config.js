002d0cb166799b88d97e01b9ccfc419863f65f7a
linzyspace <linzyspace@gmail.com>
2025-11-22 10:38:06 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
+// config/config.js
 module.exports = {
-  BLOG_ID: "6604544777764868713",
-  API_KEY: "AIzaSyCaUZEDFK50huQDi-WLKjUwOHEp0h",
+  BLOG_ID: process.env.BLOG_ID,
+  API_KEY: process.env.BLOGGER_API_KEY,
   PORT: process.env.PORT || 3000
 };
