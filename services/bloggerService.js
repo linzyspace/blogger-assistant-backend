b3d1bee8159ce5e58344814c7234c9664fd3f328
linzyspace <linzyspace@gmail.com>
2025-11-22 13:38:48 -0800

    Update bloggerService.js

--- services/bloggerService.js
+++ services/bloggerService.js
@@ -1,29 +1,28 @@
 const axios = require("axios");
-const { BLOG_ID, API_KEY } = require("../config/config");
+const { BLOG_ID, API_KEY } = require("../config");
 
-let cachedPosts = [];
-
-exports.getAllPosts = async () => {
-  if (cachedPosts.length > 0) return cachedPosts;
-
+async function fetchPosts() {
   try {
-    const url = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?key=${API_KEY}&maxResults=500`;
+    const url = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?key=${API_KEY}`;
     const res = await axios.get(url);
-
-    if (!res.data.items) {
-      console.error("Blogger API returned no posts:", res.data);
-      return [];
-    }
-
-    cachedPosts = res.data.items.map(post => ({
-      title: post.title,
-      content: post.content,
-      url: post.url
-    }));
-
-    return cachedPosts;
+    return res.data.items || [];
   } catch (err) {
-    console.error("Error fetching Blogger posts:", err.response ? err.response.data : err.message);
+    console.error("Error fetching Blogger posts:", err.message);
     return [];
   }
-};
+}
+
+function findMatchingPost(message, posts) {
+  const lowerMsg = message.toLowerCase();
+  for (let post of posts) {
+    const title = post.title.toLowerCase();
+    const content = post.content.replace(/<[^>]+>/g, "").toLowerCase();
+    if (lowerMsg.includes(title) || content.includes(lowerMsg)) {
+      const snippet = content.length > 200 ? content.substring(0, 200) + "..." : content;
+      return `From your blog post "${post.title}": ${snippet}`;
+    }
+  }
+  return null;
+}
+
+module.exports = { fetchPosts, findMatchingPost };
