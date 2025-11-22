71162f1918293ea5e9defdea1bae36f3b5553553
linzyspace <linzyspace@gmail.com>
2025-11-22 10:04:48 -0800

    Update and rename backend/config.js to backend/config/config.js

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-
-export const BLOG_ID = "6604544777764868713";
-export const API_KEY = "AIzaSyCaUZEDFK50huQDi-WLKjUwOHEp0h";
---
+++ backend/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BLOG_ID: "6604544777764868713",
+  API_KEY: "AIzaSyCaUZEDFK50huQDi-WLKjUwOHEp0h",
+  PORT: process.env.PORT || 3000
+};
