c0fa1b76b90acb5a0c8655fe0fc00397d859b6c5
linzyspace <linzyspace@gmail.com>
2025-11-22 13:52:17 -0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 5000,
-  BLOG_ID: process.env.BLOG_ID || 'YOUR_BLOGGER_BLOG_ID',
-  API_KEY: process.env.API_KEY || 'YOUR_GOOGLE_API_KEY',
+  BLOG_ID: process.env.BLOG_ID,
+  API_KEY: process.env.API_KEY
 };
