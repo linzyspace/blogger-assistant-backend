8e8259fd816603b1b60314c0fa9e9e6c812abb8c
linzyspace <linzyspace@gmail.com>
2025-11-23 18:03:04 -0800

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { chatHandler } = require("../controllers/chatController");
-
 const router = express.Router();
+const { searchAllContent } = require("./services/bloggerService");
 
-router.post("/chat", chatHandler);
+router.post("/chat", async (req, res) => {
+  const msg = req.body.message;
+
+  try {
+    const reply = await searchAllContent(msg);
+    res.json({ reply });
+  } catch (err) {
+    console.error(err);
+    res.json({ reply: "Server error while searching your blog." });
+  }
+});
 
 module.exports = router;
